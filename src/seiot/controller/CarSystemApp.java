33ea33eadecfc5fbc094422c78ef2b33b7a15373
Gianni Savini <gianni.savini@studio.unibo.it>
2018-07-29 23:32:16 +0200

    Implementata classe con il main

--- src/seiot/controller/CarSystemApp.java
+++ src/seiot/controller/CarSystemApp.java
@@ -1,30 +1,70 @@
 package seiot.controller;
 
-import java.util.*;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+import javax.swing.SwingUtilities;
+
+import seiot.model.CarSystemModel;
+import seiot.modulo_lab_2_2.msg.CommChannel;
+import seiot.modulo_lab_2_2.msg.SerialCommChannel;
+import seiot.view.CarSystemGUI;
+import seiot.view.CarSystemView;
 
 /**
  * 
  */
 public class CarSystemApp implements CarSystemViewObserver {
 
+    private CarSystemModel m;
+    private CarSystemView gui;
+    private Agent agent;
+    
+    public CarSystemApp(final CommChannel channel) {
+        
+        this.m = new CarSystemModel(channel);
+        this.gui = new CarSystemGUI(this);
+        this.gui.setObserver(this);
+        this.gui.start();
+        this.agent = new Agent();
+        agent.start();
+    }
+
     @Override
     public List<String> getAviableCommands() {
-        // TODO Auto-generated method stub
-        return null;
+         return m.getAviableCommands();
     }
 
     @Override
     public void sendMsg(String command) {
-        // TODO Auto-generated method stub
-        
+        m.getCommands().get(command).accept(m);
     }
 
     @Override
     public String getInfo() {
-        // TODO Auto-generated method stub
-        return null;
+        return m.getInfo();
     }
 
-   
-
+    private class Agent extends Thread {
+        public void run() {
+            while(true) {
+                try {
+                    SwingUtilities.invokeAndWait(()->gui.setDisplay(m.getInfo()));
+                } catch (InvocationTargetException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+                
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        CommChannel channel = new SerialCommChannel("COM5", 9600);
+        new CarSystemApp(channel);
+     }
 }
